315d0a32241577f36e2de102a281ad97870e9900
AndyGeek <andy.eulogio@gmail.com>
2020-10-07 17:08:06 -0500

    Mouse move without unmount

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -2,7 +2,10 @@ import React, { useEffect } from "react";
 
 export const Message = () => {
   useEffect(()=>{
-    console.log('Componente montado');
+    window.addEventListener('mousemove', (e)=>{
+      const coors = {x: e.x, y:e.y}
+      console.log(coors);
+    })
     return () => {
       console.log('Componente desmontado');
     }
