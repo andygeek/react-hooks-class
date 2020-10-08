d465c6dc45f3cc40a72fafb6198b72be03fcb119
AndyGeek <andy.eulogio@gmail.com>
2020-10-08 11:12:36 -0500

    The real case of useRef for useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const useFetch = (url) => {
+  const isMounted = useRef(true);
+
   const [state, setState] = useState({
     data: null,
     loading: true,
@@ -8,16 +10,26 @@ export const useFetch = (url) => {
   });
 
   useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
     setState({ data: null, loading: true, error: null });
 
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
-        setState({
-          loading: false,
-          error: null,
-          data: data,
-        });
+        if (isMounted.current) {
+          setState({
+            loading: false,
+            error: null,
+            data: data,
+          });
+        } else {
+          console.log("SetState no se llamó");
+        }
       });
   }, [url]);
 
