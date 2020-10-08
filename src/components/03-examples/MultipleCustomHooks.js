7aa878519b38f69cde45470955c1671ae840535d
AndyGeek <andy.eulogio@gmail.com>
2020-10-07 20:03:24 -0500

    Already exist useFetch

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import {useFetch} from '../../hooks/useFetch';
-import './style.css'
+import React from "react";
+import { useFetch } from "../../hooks/useFetch";
+import "./style.css";
 
 export const MultipleCustomHooks = () => {
+  const { loading, data } = useFetch(`https://www.breakingbadapi.com/api/quotes/2`);
+  const {author, quote} = !!data && data[0]; 
 
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/2`)
-    console.log(state);
-
-    return (
-        <div>
-            <h1>Custom Hooks!!</h1>
-            <hr/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Breaking Bad Quotes</h1>
+      <hr />
+      {loading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-right">
+          <p className="mb-8">{quote}</p>
+          <footer className="blockquote-footer">{author}</footer>
+        </blockquote>
+      )}
+    </div>
+  );
+};
